516f7df705bdd2cdfc32e176cc74840962cef7b7
fujikawa <?=utfpr#01brasil=?>
2023-01-24 15:50:18 -0300

    Implementação de CreatedUserHandler.

--- KafkaWithAvro.Domain/Commands/Handlers/CreateUserHandler.cs
+++ KafkaWithAvro.Domain/Commands/Handlers/CreateUserHandler.cs
@@ -1,12 +1,24 @@
 ﻿using KafkaWithAvro.Domain.Entities;
+using KafkaWithAvro.Domain.MessageBus.Producers;
 
 namespace KafkaWithAvro.Domain.Commands.Handlers
 {
     public class CreateUserHandler : ICommandHandler<CreateUserCommand>
     {
+        private readonly IProducer<User> _producer;
+
+        public CreateUserHandler(IProducer<User> producer)
+        {
+            _producer = producer;
+        }
+
         public async Task HandleAsync(CreateUserCommand request)
         {
             var newUser = new User(request.Name, request.Email);
+
+            //Execução de rotinas de cadastro
+
+            await _producer.ProduceAsync("created-user", newUser);
 
             await Task.CompletedTask;
         }
