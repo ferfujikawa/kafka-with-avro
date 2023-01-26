51a8c0d795d2f4176a4f3269a55ffbc9ef7ab22c
ferfujikawa@gmail.com <ferfujikawa@gmail.com>
2023-01-26 07:05:26 -0300

    Refatoração com uso de Extensions.

---
+++ KafkaWithAvro.Consumer.App/Extensions/AppExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using KafkaWithAvro.Consumer.App.Services;
+using KafkaWithAvro.Domain.MessageBus.Consumers.Dtos;
+using KafkaWithAvro.Domain.MessageBus.Consumers;
+using KafkaWithAvro.Infra.Kafka.Configurations;
+using KafkaWithAvro.Infra.Kafka.Consumers;
+using KafkaWithAvro.Infra.Kafka.SchemaRegistry.Client;
+using KafkaWithAvro.Infra.Kafka.SchemaRegistry.Deserializers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace KafkaWithAvro.Consumer.App.Extensions
+{
+    public static class AppExtensions
+    {
+        public static void AddKafkaSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            var kafkaConfig = new KafkaConfig();
+            configuration.GetSection("KafkaSettings").Bind(kafkaConfig);
+            services.AddSingleton(x => kafkaConfig);
+            services.AddSingleton<ISchemaRegistryClient>(x => new SchemaRegistryClient(kafkaConfig.SchemaServer));
+        }
+
+        public static void AddKafkaComponents(this IServiceCollection services)
+        {
+            services.AddSingleton<IAsyncDeserializer<UserDto>, CreateUserDeserializer>();
+            services.AddSingleton(typeof(IConsumer<>), typeof(Consumer<>));
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ConsumeCreatedUserService>();
+        }
+
+        public static async Task StartServicesAsync(this IServiceProvider services, IHostApplicationLifetime lifetime)
+        {
+            var service = services.GetRequiredService<ConsumeCreatedUserService>();
+            await service.StartAsync(lifetime.ApplicationStopping);
+        }
+    }
+}
--- KafkaWithAvro.Consumer.App/Program.cs
+++ KafkaWithAvro.Consumer.App/Program.cs
@@ -1,34 +1,20 @@
-﻿using Confluent.Kafka;
-using Confluent.SchemaRegistry;
-using KafkaWithAvro.Consumer.App.Services;
-using KafkaWithAvro.Domain.MessageBus.Consumers;
-using KafkaWithAvro.Domain.MessageBus.Consumers.Dtos;
-using KafkaWithAvro.Infra.Kafka.Configurations;
-using KafkaWithAvro.Infra.Kafka.Consumers;
-using KafkaWithAvro.Infra.Kafka.SchemaRegistry.Client;
-using KafkaWithAvro.Infra.Kafka.SchemaRegistry.Deserializers;
+﻿using KafkaWithAvro.Consumer.App.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateDefaultBuilder(args);
 
-IConfiguration config = new ConfigurationBuilder()
+var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .AddEnvironmentVariables()
     .Build();
 
 builder.ConfigureServices(services =>
 {
-    var kafkaConfig = new KafkaConfig();
-    config.GetSection("KafkaSettings").Bind(kafkaConfig);
-    services.AddSingleton(x => kafkaConfig);
-    services.AddSingleton<ISchemaRegistryClient>(x => new SchemaRegistryClient(kafkaConfig.SchemaServer));
-    services.AddSingleton<IAsyncDeserializer<UserDto>, CreateUserDeserializer>();
-
-    services.AddSingleton(typeof(IConsumer<>), typeof(Consumer<>));
-
-    services.AddSingleton<ConsumeCreatedUserService>();
+    services.AddKafkaSettings(config);
+    services.AddKafkaComponents();
+    services.AddServices();
 });
 
 using (var host = builder.Build())
@@ -36,8 +22,7 @@ using (var host = builder.Build())
     await host.StartAsync();
     var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
 
-    var service = host.Services.GetRequiredService<ConsumeCreatedUserService>();
-    await service.StartAsync(lifetime.ApplicationStopping);
+    await host.Services.StartServicesAsync(lifetime);
 
     await host.WaitForShutdownAsync(lifetime.ApplicationStopping);
 }
